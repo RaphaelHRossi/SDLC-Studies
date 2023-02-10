cc3efdd73b82ac7297029d5ffef2aa450d2887d7
RaphaelHRossi <raphaelhrossi@hotmail.com>
2023-02-10 15:01:25 -0300

    Adding current day label

--- Clock.py
+++ Clock.py
@@ -11,12 +11,17 @@ class Clock(tk.Tk):
         
         self.label = tk.Label(self, font=("Helvetica", 60), bg="grey", fg="white", justify='center', anchor='center')
         self.label.place(relx=0.5, rely=0.5, anchor="center" )
-        
+
+        self.label2 = tk.Label(self, font=("Helvetica", 30), bg="grey", fg="white", justify='center', anchor='center')
+        self.label2.place(relx=0.5, rely=0.6, anchor="center")
+
         self.update_clock()
 
     def update_clock(self):
         current_time = time.strftime("%I:%M:%S %p")
+        current_day = time.strftime("%d/%m")
         self.label.configure(text=current_time)
+        self.label2.configure(text="Dia " + current_day)
         self.after(1000, self.update_clock)
 
 if __name__ == "__main__":
